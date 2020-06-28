f63a9d4dd584b1a9b2789f358378b17b3873cc48
duinomaker <duinomaker@gmail.com>
2020-06-28 14:15:36 +0800

    Test

--- assets/test.js
+++ assets/test.js
@@ -3,12 +3,12 @@ xhr.open("GET", "https://www.toggl.com/reports/api/v2/weekly.pdf?user_agent=duin
 xhr.setRequestHeader("authorization", "Basic YzhlYTZjODVhZTQ1NThkMjQwMGJiZjM5MDk2M2U3M2Y6YXBpX3Rva2Vu");
 xhr.onreadystatechange = function () {
     console.log("(" + xhr.readyState + ", " + xhr.status + ")");
-    if (xhr.readyState == 4) {
-        if (xhr.status == 200) {
-            console.log(JSON.parse(xhr.response));
-        } else {
-            console.log(xhr);
-        }
-    }
+    // if (xhr.readyState == 4) {
+    //     if (xhr.status == 200) {
+    //         console.log(JSON.parse(xhr.response));
+    //     } else {
+    //         console.log(xhr);
+    //     }
+    // }
 }
 xhr.send();
